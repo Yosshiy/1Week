828dfbbb55614294013cd27f7fa522499ac8c798
MSI\Owner <ore329@icloud.com>
2023-03-20 22:16:01 +0900

    FirstCommit

---
+++ Assets/Project/Script/Gimmick.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Gimmick : MonoBehaviour
+{
+    [SerializeField] GameObject KO;
+    public void Volt()
+    {
+        var poy = KO.transform.eulerAngles + new Vector3(0,90,0);
+        KO.transform.DORotate(poy,1);
+    }
+}
---
+++ Assets/Project/Script/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UniRx;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    bool Juuden = false;
+
+
+    void Start()
+    {
+        Observable.EveryUpdate()
+                  .Where(x => Input.GetKeyDown(KeyCode.W))
+                  .Subscribe(x => OnMove(Vector3.forward));
+        Observable.EveryUpdate()
+                  .Where(x => Input.GetKeyDown(KeyCode.S))
+                  .Subscribe(x => OnMove(Vector3.back));
+        Observable.EveryUpdate()
+                  .Where(x => Input.GetKeyDown(KeyCode.A))
+                  .Subscribe(x => OnMove(Vector3.left));
+        Observable.EveryUpdate()
+                  .Where(x => Input.GetKeyDown(KeyCode.D))
+                  .Subscribe(x => OnMove(Vector3.right));
+    }
+
+    private void OnMove(Vector3 euler)
+    {
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position,euler,1))
+        {
+            transform.position += Vector3.up;
+        }
+
+        if (Physics.Raycast(transform.position + euler,Vector3.down,out hit,2))
+        {
+            if (Vector3.Distance(hit.point, transform.position + euler) > 1f)
+            {
+                Debug.Log(Vector3.Distance(hit.point, transform.position + euler));
+                transform.position += Vector3.down; 
+            }
+
+            transform.position += euler;
+        }
+
+        JuudenJ();
+    }
+
+    void JuudenJ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position,Vector3.down,out hit,1))
+        {
+            if(hit.transform.gameObject.tag == "Sender")
+            {
+                Juuden = !Juuden;
+                if(Juuden == false)
+                {
+                    var gim = hit.transform.gameObject.GetComponent<Gimmick>();
+                    gim.Volt();
+                }
+            }
+        }
+    }
+
+
+}
